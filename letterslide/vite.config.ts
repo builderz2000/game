d9bc2bf319df09ec6eb75fe6ca7fe4e71225cb9d
builderz2000 <79237290+builderz2000@users.noreply.github.com>
2025-10-21 11:25:31 -0500

    push 2

--- letterslide/vite.config.ts
+++ letterslide/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/game/',   // <-- replace with your repo name
+  base: '/game/letterslide',   // <-- replace with your repo name
 })
